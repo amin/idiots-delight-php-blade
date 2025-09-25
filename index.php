275d039d3112c3bd0cdd01edf1423c212b23b389
Amin El-Rifai <amin@users.noreply.github.com>
2025-09-25 21:00:03 +0200

    UPDATE: Restructured index.php file for improved readibility.

--- index.php
+++ index.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-const VIEWS_PATH = __DIR__ . '/Views';
-const CACHE_PATH = __DIR__ . '/Views/Cache';
-
 use Core\Game;
 use Session\GameSession;
-
 use eftec\bladeone\BladeOne;
+
+const VIEWS_PATH = __DIR__ . '/Views';
+const CACHE_PATH = __DIR__ . '/Views/Cache';
+$blade = new BladeOne(VIEWS_PATH, CACHE_PATH, BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
 
 $game = new Game();
 $gameArray = $game->dealRound();
-
-$blade = new BladeOne(VIEWS_PATH, CACHE_PATH, BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
-
 // Blade test.
 echo $blade->run("game", ["Hello" => "World"]);
