e871f002663a0c2d8ece1d4aff917d37919b12b3
Amin El-Rifai <amin@users.noreply.github.com>
2025-09-25 20:51:26 +0200

    UPDATE: Installation of composer, blade template engine.

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/Core/Card.php';
-require_once __DIR__ . '/Core/Deck.php';
-require_once __DIR__ . '/Core/Game.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+const VIEWS_PATH = __DIR__ . '/Views';
+const CACHE_PATH = __DIR__ . '/Views/Cache';
+
+use Core\Game;
+use eftec\bladeone\BladeOne;
 
 $game = new Game();
+$gameArray = $game->dealRound();
+
+$blade = new BladeOne(VIEWS_PATH, CACHE_PATH, BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
+
+// Blade test.
+echo $blade->run("game", ["Hello" => "World"]);
