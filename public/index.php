ab4915c0e7ffb7e158fac2c601f1013c0cf0b3e8
Amin El-Rifai <amin@users.noreply.github.com>
2025-09-25 21:34:23 +0200

    Renamed folders following a modern laravel convention.

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@ use Core\Game;
 use Session\GameSession;
 use eftec\bladeone\BladeOne;
 
-const VIEWS_PATH = __DIR__ . '/../src/Views';
-const CACHE_PATH = __DIR__ . '/../src/Views/Cache';
+const VIEWS_PATH = __DIR__ . '/../app/Views';
+const CACHE_PATH = __DIR__ . '/../app/Views/Cache';
 $blade = new BladeOne(VIEWS_PATH, CACHE_PATH, BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
 
 $game = new Game();
