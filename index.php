9eded2c001b3403f60c41a42ed4a536183b09cc3
Amin El-Rifai <amin@users.noreply.github.com>
2025-09-25 14:28:52 +0200

    UPDATE: Updates requires to mirror new directory structure.

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/core/Deck.php';
-require_once __DIR__ . '/core/Card.php';
+require_once __DIR__ . '/Core/Deck.php';
+require_once __DIR__ . '/Core/Card.php';
 
 
 $deck = new Deck();
